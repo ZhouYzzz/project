bbe2e44c8f3066eb32deedd0aafc8eb926dc9934
ZhouYzzz <zhouyz9608@gmail.com>
2016-04-23 22:14:26 +0800

    [work]

--- database/VIPeR/viper.py
+++ database/VIPeR/viper.py
@@ -1,46 +1,48 @@
-def method(im):
-    """ im -> feat """
-    feat = im
-    return feat
+#!/usr/bin/python
 
 import pandas as pd
-import numpy as np
 import os
-from skimage.io import imread
+import numpy as np
+from cv2 import imread, resize
 
 _TFP = os.path.dirname(__file__)
-A_TXT = 'a.txt'
-B_TXT = 'b.txt'
 
-def iread(iname, dir=_TFP):
-    im = imread(os.path.join(dir,iname))
-    return im.transpose((2,0,1))
+def iread(imf,root=_TFP):
+    im = imread(os.path.join(root,imf))
+    assert im is not None
+    im = resize(im, (128,256))
+    return im.transpose((2,0,1))[np.newaxis,...]
 
 class VIPeR():
     def __init__(self):
-        self.A = pd.read_csv(A_TXT, header=None)[0].tolist()
-        self.B = pd.read_csv(B_TXT, header=None)[0].tolist()
-        self.C, self.H, self.W = iread(self.A[0]).shape
-        print self.C, self.H, self.W
+        self.A = pd.read_csv('a.txt',sep=' ',header=None)[0].tolist()
+        self.B = pd.read_csv('b.txt',sep=' ',header=None)[0].tolist()
+        assert (len(self.A) == len(self.B))
+        self.load()
+        pass
 
     def load(self):
-        # print A, B
-        pass
+        print '[VIPeR] loading'
+        self.dA = list()
+        self.dB = list()
+        for iA in self.A:
+            self.dA.append(iread(iA))
+        for iB in self.B:
+            self.dB.append(iread(iB))
 
-    def preprocess(self):
-        pass
+        self.dA = np.vstack(self.dA)
+        self.dB = np.vstack(self.dB)
+        print '[VIPeR] loading finished', self.dA.shape
+        (self.N,self.C,self.H,self.W) = self.dA.shape
 
-    def test(self):
-        testidx = np.random.choice(632, 316, False)
-        print testidx
-        pass
+    def get_test(self):
+        randidx = np.random.randint(0,632,316)
+        query = self.dA[randidx]
+        gallery = self.dB[randidx]
+        return query, gallery
 
-import os
-#print len(os.listdir('cam_a'))
-#print len(os.listdir('cam_b'))
 
-"""Cross database"""
+if __name__ == "__main__":
+    db = VIPeR()
+    db.get_test()
 
-a = VIPeR()
-a.load()
-a.test()
