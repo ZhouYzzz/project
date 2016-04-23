9ecff80bcc00200851aedb0c0c10ca4ca7f1fea3
ZhouYzzz <zhouyz9608@gmail.com>
2016-04-23 23:59:22 +0800

    viper valid

--- database/VIPeR/viper.py
+++ database/VIPeR/viper.py
@@ -4,6 +4,7 @@ import pandas as pd
 import os
 import numpy as np
 from cv2 import imread, resize
+from utils import CHECK, crop
 
 _TFP = os.path.dirname(__file__)
 
@@ -15,6 +16,7 @@ def iread(imf,root=_TFP):
 
 class VIPeR():
     def __init__(self):
+        """ VIPeR only have 2 images for each identity, one in a view """
         self.A = pd.read_csv('a.txt',sep=' ',header=None)[0].tolist()
         self.B = pd.read_csv('b.txt',sep=' ',header=None)[0].tolist()
         assert (len(self.A) == len(self.B))
@@ -36,10 +38,52 @@ class VIPeR():
         (self.N,self.C,self.H,self.W) = self.dA.shape
 
     def get_test(self):
+        """ for test purpose """
         randidx = np.random.randint(0,632,316)
         query = self.dA[randidx]
         gallery = self.dB[randidx]
         return query, gallery
+
+    def getA(self, idx):
+        """ get im from A """
+        if isinstance(idx, int):
+            return self.dA[[idx]]
+        else:
+            return self.dA[idx]
+
+    def getB(self, idx):
+        """ get im from B """
+        if isinstance(idx, int):
+            return self.dB[[idx]]
+        else:
+            return self.dB[idx]
+
+    def _gen_sim(self, idx, crop=True, cropsize=(240,120)):
+        A = self.getA(idx)
+        B = self.getB(idx)
+        AB = np.concatenate((A,B),axis=1)
+        if crop: return crop(AB, cropsize[0], cropsize[1])
+        else: return AB
+
+    def _gen_dif(self, idx, crop=True, cropsize=(240,120)):
+        A = self.getA(idx)
+        B = self.getB(self._rand_not_idx(idx))
+        AB = np.concatenate((A,B),axis=1)
+        if crop: return crop(AB, cropsize[0], cropsize[1])
+        else: return AB
+    
+    def gen_valid(self, batch=128, crop=True, cropsize=(240,120)):
+        randidx = np.random.randint(self.N, batch/2)
+        pair_batch = list()
+        for idx in randidx:
+            pair_batch.append(self._gen_sim(idx, crop, cropsize))
+            pair_batch.append(self._gen_dif(idx, crop, cropsize))
+        return np.vstack(pair_batch)
+
+    def _rand_not_idx(self, idx):
+        r = np.random.randint(self.N)
+        if (r == idx): return self._rand_not_idx(idx)
+        else: return r
 
 
 if __name__ == "__main__":
--- test/test.py
+++ test/test.py
@@ -1,16 +1,19 @@
 import numpy as np
 from time import time
 
-ITER = 1000
-a = np.zeros([100,200,200],dtype=np.uint8)
+# ITER = 1000
+# a = np.zeros([100,200,200],dtype=np.uint8)
 
-t = time()
-for i in xrange(ITER):
-    a = np.zeros([100,200,200],dtype=np.uint8)
-print (time() - t)/ITER;
+# t = time()
+# for i in xrange(ITER):
+#     a = np.zeros([100,200,200],dtype=np.uint8)
+# print (time() - t)/ITER;
 
-t = time()
-b = np.ones([100,200,200],dtype=np.uint8)
-for i in xrange(ITER):
-    np.copyto(a,b)
-print (time() - t)/ITER;
+# t = time()
+# b = np.ones([100,200,200],dtype=np.uint8)
+# for i in xrange(ITER):
+#     np.copyto(a,b)
+# print (time() - t)/ITER;
+
+if (1==2): print True
+else: print False
