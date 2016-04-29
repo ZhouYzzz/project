bede72fc9646cff07689b363e7a93b8bf021288d
ZhouYzzz <zhouyz9608@gmail.com>
2016-04-29 11:48:19 +0800

    server and tools

--- layer/__init__.py
+++ layer/__init__.py
@@ -1,5 +1,10 @@
+# Data Layers
 from data_layer import DataLayer
 from metric_data_layer import MetricDataLayer
 from valid_metric_data_layer import ValidMetricDataLayer
 from label_data_layer import LabelDataLayer
 from triplet_data_layer import TripletDataLayer
+
+# Functional Layers
+from analyse_layer import AnalyseLayer
+from split_layer import SplitLayer
