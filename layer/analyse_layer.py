5443877906c061a8c79d5c50edc5cf429fa3abb8
zhouyz14 <zhouyz14@zhouyz14.deephi.nics.info>
2016-04-30 21:07:31 +0800

    [analyse]

--- layer/analyse_layer.py
+++ layer/analyse_layer.py
@@ -2,30 +2,34 @@ import caffe
 import numpy as np
 import pandas as pd
 from utils import CHECK
+import matplotlib.pyplot as plt
 
 class AnalyseLayer(caffe.Layer):
     '''Do analisis on result'''
     def setup(self, bottom, top):
-        self.N = bottom[0].data.shape
+        self.N = bottom[0].data.shape[0]
         pass
 
     def reshape(self, bottom, top):
+        #top[0].reshape(self.N)
+        #top[1].reshape(self.N)
         pass
 
     def forward(self, bottom, top):
         anchor = bottom[0].data
         pair = bottom[1].data
-        label = bottom[2].data
+        label = bottom[2].data.reshape(-1)
+        print anchor.shape, pair.shape
 
         diff = np.linalg.norm(anchor-pair,ord=2,axis=1)
 
-        dist_sim = list()
-        dist_diff= list()
-        for i in xrange(self.N):
-            if label[i]: # sim pair
-                dist_sim.append(diff[i])
-            else: # diff pair
-                dist_diff.append(diff[i])
+        dist_sim = diff[label.astype(bool)]
+        dist_diff = diff[np.logical_not(label.astype(bool))]
+
+        top[0].reshape(*dist_sim.shape)
+        top[0].data[...] = dist_sim
+        top[1].reshape(*dist_diff.shape)
+        top[1].data[...] = dist_diff
 
     def backward(self, top, propagate_down, bottom):
         pass
